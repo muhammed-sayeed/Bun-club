ebde47de5f1a1ee8d0e20eb762ef1d3e7381edb0
muhammed-sayeed <sayeedmon25@gmail.com>
2024-03-26 21:50:09 +0530

    Responsivness almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,34 @@ const blurHeader = ()=>{
                        : header.classList.remove('blur-header')
 }
 window.addEventListener('blur', blurHeader)
+
+//================ Show Scroll Up ==================================
+const scrollUp = ()=>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+
+//============ Scroll section ative Link ===========================
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = ()=>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId+']')
+
+              if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+                sectionClass.classList.add('active-link')
+              } else {
+                sectionClass.classList.remove('active-link')
+              }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
